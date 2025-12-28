30fd13ce52ae7eea3f57a0ed5d04f890749c9ff8
MarianaG822 <166176484+MarianaG822@users.noreply.github.com>
2025-12-28 06:18:18 -0300

    Refactor import statements to use relative paths
    Updated import paths to use relative paths instead of absolute paths.

--- App.tsx
+++ App.tsx
@@ -1,10 +1,9 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "./toaster";
+import { Toaster as Sonner } from "./sonner";
+import { TooltipProvider } from "./tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "/Index";
-import NotFound from "/NotFound";
+import Index from "./Index"; // Ajustado para ler da raiz
 
 const queryClient = new QueryClient();
 
@@ -16,8 +15,6 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
@@ -25,3 +22,4 @@ const App = () => (
 );
 
 export default App;
+
